084ed48a112d3025d3de660af0f011f8df54eab2
Ryo Narisawa <ororoauto@gmail.com>
2021-01-30 19:32:44 +0900

    Initial Commit

---
+++ ComparePrices/ComparePricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComparePricesApp.swift
+//  ComparePrices
+//
+//  Created by Ryo Narisawa on 2021/01/30.
+//
+
+import SwiftUI
+
+@main
+struct ComparePricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
