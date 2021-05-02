6c1437a937108794c4341792d19eba5e08f7c605
Ryo Narisawa <ororoauto@gmail.com>
2021-05-02 23:50:15 +0900

    add R.swift

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     platforms: [.macOS(.v10_15)],
     dependencies: [
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.41.2"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.43.1")
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.43.1"),
+        .package(url: "https://github.com/mac-cain13/R.swift.git", from: "5.4.0"),
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
